0a13e61b8d8d4f858e808eec9584a5ae4dfcbe21
Akshy Kumar <akhi9573c@gmail.com>
2023-07-04 20:08:01 +0530

    Time: 551 ms (55.25%), Space: 33.5 MB (19.47%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,19 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        if len(nums)==1 or len(nums)==0:
-            return False
-        freq_map={}
-        #Create the freq table
-        for num in nums:
-            freq_map[num]=freq_map.get(num,0)+1
-        
-        #iterate over the hashmap and check if any ke maps to value greater than 1
-        for key in freq_map:
-            if freq_map[key]>1:
+        hashset = {}
+
+        for n in nums:
+            if n in hashset:
                 return True
-            
-        #if the loop terminates it means all numbers are distinct 
+            hashset[n]=hashset.get(n,0)+1
         return False
-        
-      
-        
